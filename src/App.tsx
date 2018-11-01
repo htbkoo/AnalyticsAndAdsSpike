c78c340fcd6fc874deb22e3f8af89d6f801b221b
htbkoo <h1017760@connect.hku.hk>
2018-11-01 13:33:04 +0800

    refactored to eliminate warnings htbkoo/AnalyticsAndAdsSpike

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import * as ReactGA from 'react-ga';
 import Router from './Router';
 
 function asDisplayString(value) {
-    if (typeof value === 'string') return `'${value}'`;
+    if (typeof value === 'string') {
+        return `'${value}'`;
+    }
     return value.toString();
 }
 
@@ -13,7 +15,9 @@ function renderConfigString(config, indent = '') {
     return [
         '{',
         ...Object.keys(config).map((key) => {
-            if (config[key] === undefined || config[key] === null) return '';
+            if (config[key] === undefined || config[key] === null) {
+                return '';
+            }
             return `\t${key}: ${asDisplayString(config[key])}`;
         }).filter(value => value),
         '}'
@@ -54,64 +58,7 @@ export default class App extends PureComponent<{}, AppState> {
         };
     }
 
-    private filteredConfigs = () => {
-        return this.state.configs.filter(({trackingId: id}) => id);
-    };
-
-    private initReactGA = (event: FormEvent) => {
-        event.preventDefault();
-        if (this.filteredConfigs().length === 0) {
-            return;
-        }
-        ReactGA.initialize(this.state.configs);
-        // Send initial test view
-        ReactGA.pageview('test-init-pageview');
-        this.setState({reactGaInitialised: true});
-    };
-
-    private addConfig = () => {
-        this.setState({
-            configs: [...this.state.configs, DEFAULT_CONFIG]
-        });
-    };
-
-    private updateConfig = (configIndex, key, type, event) => {
-        const config = this.state.configs[configIndex];
-        let value;
-        if (type === 'checkbox') {
-            value = !config[key];
-        } else {
-            value = event.target.value;
-        }
-        const newConfig = {
-            ...config,
-            [key]: value
-        };
-        this.setState({
-            configs: [
-                ...this.state.configs.slice(0, configIndex),
-                newConfig,
-                ...this.state.configs.slice(configIndex + 1)
-            ]
-        });
-    };
-
-    renderConfigs = () => {
-        const configs = this.filteredConfigs();
-        if (configs.length === 0) return '';
-        if (configs.length === 1) {
-            const {trackingId, ...options} = configs[0];
-            return `'${trackingId}'${Object.keys(options).map(key => options[key]).filter(val => val).length ? `, ${JSON.stringify(options)}` : ''}`;
-        }
-        return `[\n${
-            configs.reduce((result, config, index, array) => {
-                const configString = renderConfigString(config, '\t');
-                return `${result}${result ? '\n' : ''}${configString}${index < array.length - 1 ? ',' : ''}`;
-            }, '')
-            }\n]`;
-    };
-
-    render() {
+    public render() {
         const {configs, reactGaInitialised} = this.state;
         if (reactGaInitialised) {
             return (
@@ -155,4 +102,63 @@ export default class App extends PureComponent<{}, AppState> {
             </form>
         );
     }
+
+    private filteredConfigs = () => {
+        return this.state.configs.filter(({trackingId: id}) => id);
+    };
+
+    private initReactGA = (event: FormEvent) => {
+        event.preventDefault();
+        if (this.filteredConfigs().length === 0) {
+            return;
+        }
+        ReactGA.initialize(this.state.configs);
+        // Send initial test view
+        ReactGA.pageview('test-init- pageview');
+        this.setState({reactGaInitialised: true});
+    };
+
+    private addConfig = () => {
+        this.setState({
+            configs: [...this.state.configs, DEFAULT_CONFIG]
+        });
+    };
+
+    private updateConfig = (configIndex, key, type, event) => {
+        const config = this.state.configs[configIndex];
+        let value;
+        if (type === 'checkbox') {
+            value = !config[key];
+        } else {
+            value = event.target.value;
+        }
+        const newConfig = {
+            ...config,
+            [key]: value
+        };
+        this.setState({
+            configs: [
+                ...this.state.configs.slice(0, configIndex),
+                newConfig,
+                ...this.state.configs.slice(configIndex + 1)
+            ]
+        });
+    };
+
+    private renderConfigs = () => {
+        const configs = this.filteredConfigs();
+        if (configs.length === 0) {
+            return '';
+        }
+        if (configs.length === 1) {
+            const {trackingId, ...options} = configs[0];
+            return `'${trackingId}'${Object.keys(options).map(key => options[key]).filter(val => val).length ? `, ${JSON.stringify(options)}` : ''}`;
+        }
+        return `[\n${
+            configs.reduce((result, config, index, array) => {
+                const configString = renderConfigString(config, '\t');
+                return `${result}${result ? '\n' : ''}${configString}${index < array.length - 1 ? ',' : ''}`;
+            }, '')
+            }\n]`;
+    };
 }
